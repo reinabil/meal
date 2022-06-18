b96bc83554d198048d9e034dcc701695a4b4f97b
vel-adr <anselmus.pavel@gmail.com>
2022-06-18 12:01:40 +0700

    creating and connecting view controller for signIn and joinCreateFamily page

---
+++ Meal/Controllers/SignInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignInViewController.swift
+//  Meal
+//
+//  Created by Anselmus Pavel Adriska on 18/06/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //Continue with Apple button pressed
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+}
