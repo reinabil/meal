f14061a97469015153d769472aea6103ea1ee7ac
vel-adr <anselmus.pavel@gmail.com>
2022-06-23 11:29:32 +0700

    update Member page UI

--- Meal/Views/CustomCells/FamilyIDTableViewCell.swift
+++ Meal/Views/CustomCells/FamilyIDTableViewCell.swift
@@ -14,6 +14,7 @@ class FamilyIDTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        familyIDLabel.textColor = .black
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Meal/Views/CustomCells/UserNameTableViewCell.swift
+++ Meal/Views/CustomCells/UserNameTableViewCell.swift
@@ -12,9 +12,13 @@ class UserNameTableViewCell: UITableViewCell {
     @IBOutlet weak var leftSideLabel: UILabel!
     @IBOutlet weak var rightSideLabel: UILabel!
     
+    let textGrayColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6)
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        leftSideLabel.textColor = .black
+        rightSideLabel.textColor = textGrayColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
